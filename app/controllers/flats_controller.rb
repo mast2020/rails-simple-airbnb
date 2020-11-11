c86b293f93f6eafefb990f53ec73164c261aba51
Mario Stehling <mario.stehling@gmx.de>
2020-11-11 16:54:43 +0100

    Completed Simple Airbnb

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,40 @@
+class FlatsController < ApplicationController
+  before_action :find_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    redirect_to flats_path
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Flat2',
+  address: 'Flat2Street',
+  description: 'Flat2Description',
+  price_per_night: 76,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Flat3',
+  address: 'Flat3Street',
+  description: 'Flat3Description',
+  price_per_night: 77,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Flat4',
+  address: 'Flat4Street',
+  description: 'Flat4Description',
+  price_per_night: 78,
+  number_of_guests: 6
+)
